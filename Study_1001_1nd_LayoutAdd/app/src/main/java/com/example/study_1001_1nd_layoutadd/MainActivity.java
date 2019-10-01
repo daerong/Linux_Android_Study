f55923b3f4b2b24f95ea03b2be04f2f4645eeef9
Daeseong Yu <yu5501@nate.com>
2019-10-01 10:25:07 +0900

    액티비티 전환 실습

---
+++ Study_1001_1nd_LayoutAdd/app/src/main/java/com/example/study_1001_1nd_layoutadd/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.study_1001_1nd_layoutadd;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnOpen;
+    Intent intent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnOpen = findViewById(R.id.btnOpen);
+        btnOpen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                intent = new Intent(getApplicationContext(), NewActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
---
+++ Study_1001_1nd_LayoutAdd/app/src/main/java/com/example/study_1001_1nd_layoutadd/NewActivity.java
@@ -0,0 +1,26 @@
+package com.example.study_1001_1nd_layoutadd;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class NewActivity extends AppCompatActivity {
+
+    Button btnBack;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new);
+
+        btnBack = findViewById(R.id.btnBack);
+
+        btnBack.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                finish();
+            }
+        });
+    }
+
+}
