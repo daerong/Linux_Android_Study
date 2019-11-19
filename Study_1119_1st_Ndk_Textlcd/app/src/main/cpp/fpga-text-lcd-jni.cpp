c56c9bb4e2026984da10b413a00334e82a3e81dc
Daeseong Yu <yu5501@nate.com>
2019-11-19 12:01:09 +0900

    2번째줄 마지막 문자 문제있음! -> 추후 수정

--- Study_1119_1st_Ndk_Textlcd/app/src/main/cpp/fpga-text-lcd-jni.cpp
+++ Study_1119_1st_Ndk_Textlcd/app/src/main/cpp/fpga-text-lcd-jni.cpp
@@ -15,7 +15,7 @@ int fpga_text_lcd(const char *str1, const char *str2){
     size_t str_size;
     int chk_size;
 
-    char string[32];
+    unsigned char string[32];
     memset(string, 0, sizeof(string));
 
     dev = open(TEXT_LCD_DEVICE, O_RDWR);
@@ -26,13 +26,13 @@ int fpga_text_lcd(const char *str1, const char *str2){
         str_size = strlen(str1);
         __android_log_print(ANDROID_LOG_INFO, "LogGo", "Driver = %d", str_size);
         if(str_size > 0){
-            strncat(string, str1, str_size);
+            strncat(reinterpret_cast<char *>(string), str1, str_size);
             memset(string + str_size, ' ', LINE_BUFF - str_size);
         }
         str_size = strlen(str2);
         __android_log_print(ANDROID_LOG_INFO, "LogGo", "Driver = %d", str_size);
         if(str_size > 0){
-            strncat(string, str2, str_size);
+            strncat(reinterpret_cast<char *>(string), str2, str_size);
             memset(string + LINE_BUFF + str_size, ' ', LINE_BUFF - str_size);
             __android_log_print(ANDROID_LOG_INFO, "LogGo", "String = %s", string);
         }
